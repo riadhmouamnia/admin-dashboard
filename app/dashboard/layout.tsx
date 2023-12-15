f994a051e6ef171bcd0d71a084dc1b5f60698af1
Riadh mouamnia <riadh.mouamnia@gmail.com>
2023-12-15 18:05:47 +0100

    initial setup

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@ui/dashboard/navbar";
+import Sidebar from "@ui/dashboard/sidebar";
+import styles from "@ui/dashboard/dashboard.module.css";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
