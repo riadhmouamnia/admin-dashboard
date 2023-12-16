13772033eacf9985844c45244be4e7e624cec623
Riadh mouamnia <riadh.mouamnia@gmail.com>
2023-12-16 15:05:44 +0100

    create products page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,3 +1,66 @@
+import Pagination from "@/app/ui/dashboard/pagination";
+import Search from "@/app/ui/dashboard/search";
+import styles from "@ui/dashboard/products/products.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Product one
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>Price</td>
+            <td>12-1-2023</td>
+            <td>75</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/dashboard/products/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type="hidden" name="id" />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 }
